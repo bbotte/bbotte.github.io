b5ae1df12cfaa5c8999175fa8392b9abc0560bea
matuoyi <517766237@qq.com>
2018-10-22 16:22:06 +0800

    按时间删除文件

---
+++ shell/delete_file_by_time.sh
@@ -0,0 +1,8 @@
+delete files by time
+按时间删除文件，或者做其他用途
+
+除了最近修改的三个文件都删除
+find . -name "*" -printf "%AY-%Am-%Ad %AH:%AM:%AS %p\n"|sort -rn|awk '{print $3}' |sed '1,4d'|grep -v "^.$"|xargs rm -rf
+
+除了最近修改的三个文件都删除，只是当前目录，没有字目录
+ls -lt|sed '1,4d'
