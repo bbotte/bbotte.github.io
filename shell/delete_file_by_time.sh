121b50e502a9910dd1660dd25905cb5626236593
bbotte <bbotte@163.com>
2018-11-01 18:09:20 +0800

    Update delete_file_by_time.sh

--- shell/delete_file_by_time.sh
+++ shell/delete_file_by_time.sh
@@ -4,5 +4,5 @@ delete files by time
 除了最近修改的三个文件都删除
 find . -name "*" -printf "%AY-%Am-%Ad %AH:%AM:%AS %p\n"|sort -rn|awk '{print $3}' |sed '1,4d'|grep -v "^.$"|xargs rm -rf
 
-除了最近修改的三个文件都删除，只是当前目录，没有字目录
+除了最近修改的三个文件都删除，只是当前目录，没有子目录
 ls -lt|sed '1,4d'
