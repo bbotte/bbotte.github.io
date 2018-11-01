351eb8a8d9836d034ba35943421d29deb2eb4c4b
bbotte <bbotte@163.com>
2018-11-01 18:20:26 +0800

    Update delete_file_by_time.sh

--- shell/delete_file_by_time.sh
+++ shell/delete_file_by_time.sh
@@ -4,8 +4,8 @@ delete files by time
 除了最近修改的三个文件都删除,包含子文件夹，按访问时间排序
 find . -name "*" -printf "%AY-%Am-%Ad %AH:%AM:%AS %p\n"|sort -rn|awk '{print $3}' |sed '1,4d'|grep -v "^.$"|xargs rm -rf
 
-当然一个文件有 Access Modify Change，如果按 Modify 时间排序
-find . -name "*" -printf "%MY-%Mm-%Md %MH:%MM:%MS %p\n"|sort -rn|awk '{print $3}' |sed '1,4d'|grep -v "^.$"|xargs rm -rf
+当然一个文件有 Access Modify Change，如果按 Change 时间排序
+find . -name "*" -printf "%CY-%Cm-%Cd %CH:%CM:%CS %p\n"|sort -rn|awk '{print $3}' |sed '1,4d'|grep -v "^.$"|xargs rm -rf
 
 
 除了最近修改的三个文件都删除，只是当前目录，没有子目录
