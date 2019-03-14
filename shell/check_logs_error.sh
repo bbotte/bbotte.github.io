32766807b2a6d0443181d149e393feb30383e904
bbotte <bbotte@163.com>
2019-03-14 10:49:56 +0800

    Update check_logs_error.sh

--- shell/check_logs_error.sh
+++ shell/check_logs_error.sh
@@ -5,7 +5,7 @@ PATH=/bin:/usr/bin:/sbin:/usr/sbin
 MYMAIL=(zhangsan@qq.com lisi@qq.com)
 errorlognum=`awk '$9=="500" || $9=="502" || $9=="503" || $9=="504" {print}' /var/log/nginx/api.access.log|egrep -v "XYZ|okhttp" |wc -l`
 errorloghead=`awk '$9=="500" || $9=="502" || $9=="503" || $9=="504" {print}' /var/log/nginx/api.access.log|awk '{printf "%s %s %s\n",$11,$6,$7}'|sed 's/"//g'|awk -F'?' '{print $1}'|sort -rn |uniq -c|sort -rn|head`
-errorlog=`awk '$9=="500" || $9=="502" || $9=="503" || $9=="504" {print}' /var/log/nginx/top-api.access.log|egrep -v "XYZ|okhttp" |tail -n 1`
+errorlog=`awk '$9=="500" || $9=="502" || $9=="503" || $9=="504" {print}' /var/log/nginx/api.access.log|egrep -v "XYZ|okhttp" |tail -n 1`
 hourtime=`echo $errorlog|awk '{print $4}'|awk -F':' '{print $1":"$2":"$3}'|awk -F'[' '{print $2}'|sed 's/.$/0/'|sed 's#/#\ #g'|sed 's/:/\ /'`
 # 这里取的时间不是精确的十分钟之前
 logtimestamp=`date +%s -d"$hourtime"`
@@ -15,9 +15,9 @@ echo $logtimestamp
 echo $nowtimestamp
 
 if [ $logtimestamp -eq $nowtimestamp ];then
-    echo -e "router-top-node1 \n top-api.sao.so \n 10分钟内有50X错误日志 \n 今天50X日志总计$errorlognum 条 \n /var/log/nginx/access.log \n $errorloghead"|mail -s "router-top-node1 top-api has 50X logs" ${MYMAIL[*]}
+    echo -e "node1 \n api.bbotte.com \n 10分钟内有50X错误日志 \n 今天50X日志总计$errorlognum 条 \n /var/log/nginx/access.log \n $errorloghead"|mail -s "router-top-node1 top-api has 50X logs" ${MYMAIL[*]}
 fi
 
 
 # write crontab
-# */10 * * * * sh /root/top-apierror.sh
+# */10 * * * * sh /root/apierror.sh
