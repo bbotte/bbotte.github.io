e5975838cb063b681d22bb1aeb5cec497a6726cb
linux工匠 <bbotte@163.com>
2024-02-20 16:31:43 +0800

    Update DeleteElasticsearchIndices.py

--- python_project/DeleteElasticsearchIndices.py
+++ python_project/DeleteElasticsearchIndices.py
@@ -2,7 +2,7 @@
 #ecoding=utf8
 #自动删除elasticsearch数据库保留较久的数据
 #普通数据保留时间为daysToKeep
-#pip install elasticsearch  安装elasticsearch包
+#pip install elasticsearch  安装elasticsearch包,注意版本要一致
 import datetime
 import re
 from elasticsearch import Elasticsearch
@@ -16,12 +16,15 @@ indices_list = []
 result = es.cat.indices()
 data = result.splitlines()
 
-test1_kibana = re.compile(r'test1')   #筛选包含test1的index
-test2_kibana = re.compile(r'test2')   #筛选包含test2的index
-lines = [line for line in data if test1_kibana.search(line) is not None or test2_kibana.search(line) is not None ]
+#test1_kibana = re.compile(r'test1')   #筛选包含test1的index
+#test2_kibana = re.compile(r'test2')   #筛选包含test2的index
+#lines = [line for line in data if test1_kibana.search(line) is not None or test2_kibana.search(line) is not None ]
 
-for i in range(len(lines)):
-    indices_list.append(re.split(r' ',lines[i])[2])
+#for i in range(len(lines)):
+#    indices_list.append(re.split(r' ',lines[i])[2])
+
+for i in range(len(data)):
+    indices_list.append(re.split(r' ',data[i])[2])
 
 for y in range(len(indices_list)):
     idx_date = re.split(r'\-',indices_list[y])[-1]
